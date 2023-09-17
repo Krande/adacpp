cf064d49c5afeff79af1cda32b73c29100da6010
krande <kristoffer_andersen@outlook.com>
2023-09-17 18:11:03 +0200

    Add color setting functionality, upgrade dependencies, CI config tweaks
    Introduced new functionality for setting colors on shapes in the 'occt' module. Upgraded dependencies in the build environment including 'occt' to 7.7.2 and 'nanobind' to 1.5.2. Improved continuous integration configuration by using 'mambabuild' in favor of 'boa build' for building local packages, removed unnecessary ninja installation step, and provided 'micromamba' setup arguments. Renamed 'conda/recipe.yaml' to 'conda/meta.yaml' and adjusted syntax for improved readability. Code files were added and updated to support the new color setting functionality.

--- src/adacpp.cpp
+++ src/adacpp.cpp
@@ -1,5 +1,6 @@
 #include "binding_core.h"
 #include "exchange/occt/step_writer.h"
+#include "exchange/occt/colors.h"
 #include "tessellation/tess_helpers.h"
 #include "exchange/tinygltf/tiny.h"
 #include "fem/simple_mesh.h"
@@ -9,6 +10,7 @@
 // Define the modules that will be exposed in python
 NB_MODULE(_ada_cpp_ext_impl, m) {
     shape_module(m);
+    occt_color_module(m);
     step_writer_module(m);
     tess_helper_module(m);
     tiny_gltf_module(m);
