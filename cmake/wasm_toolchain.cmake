4f9a8859c167fb0f0ce69b948640e2a541e77892
krande <kristoffer_andersen@outlook.com>
2024-09-07 20:24:47 +0200

    start work on adding wasm output

---
+++ cmake/wasm_toolchain.cmake
@@ -0,0 +1,12 @@
+# cmake/wasm_toolchain.cmake
+set(CMAKE_SYSTEM_NAME WebAssembly)
+set(CMAKE_SYSTEM_VERSION 1)
+
+set(CMAKE_C_COMPILER "emcc")
+set(CMAKE_CXX_COMPILER "em++")
+
+# Required to link properly for WASM
+set(CMAKE_EXE_LINKER_FLAGS "--bind")
+
+# Set output directory for WebAssembly build
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/wasm_output")
---
+++ src/wasm_utils.h
@@ -0,0 +1,8 @@
+//
+// Created by Kristoffer on 07.09.2024.
+//
+
+#ifndef WASM_UTILS_H
+#define WASM_UTILS_H
+
+#endif //WASM_UTILS_H
